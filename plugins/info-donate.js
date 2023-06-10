77e7d64653bdf29d791c7a893952d60e3dd05fd8
DjDarian <136194986+DjDarian@users.noreply.github.com>
2023-06-10 17:10:39 -0300

    Update info-donate.js

--- plugins/info-donate.js
+++ plugins/info-donate.js
@@ -10,35 +10,35 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 *┇ ᴘᴇʀᴏ sɪ ǫᴜɪᴇʀᴇs ᴘᴜᴇᴅᴇ ᴀᴘᴏʏᴀʀ ᴀʟ ʙᴏᴛ.*
 *┇ sᴜsᴄʀɪʙɪᴇɴᴅᴏsᴇ ᴀʟ ᴄᴀɴᴀʟ ᴏғɪᴄɪᴀʟ ᴅᴇʟ ʙᴏᴛ*
 *┇ ᴘᴀʀᴀ ǫᴜᴇ ᴄᴀᴅᴀ ᴠᴇᴢ sᴏᴍᴏs ᴍᴀs*
-*┇ ᴇʟ ʟᴀ ғᴀᴍɪʟɪᴀs ʟᴏʟɪʙᴏᴛ ʏ ɴᴏᴠᴀʙᴏᴛ-ᴍᴅ*
+*┇ *
 *┇ ʏᴀ ᴄᴏɴ ᴇsᴏ sᴏʏ ғᴇʟɪᴢ.😄*
 *┇ ʏ ᴀᴘᴏʏᴀʀʟᴏs ᴄᴏɴ ᴜɴᴀ 🌟 ᴇʟ ɢɪᴛʜᴜʙ ᴏғɪᴄɪᴀʟ ᴅᴇʟ ʙᴏᴛ*
 *┃* ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 *┃* ʏᴏᴜᴛᴜʙᴇ | sᴜsᴄʀɪʙɪʀᴛᴇ 🔕
-*┃*  *https://www.youtube.com/@TheLoliBot-MD*
+*┃*  *https://www.youtube.com/AGREGAR LINK
 *┃* ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 *┃*  ɢɪᴛʜᴜʙ 🌟
 *┃*  *${fgig}*
 *┃* ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 *┃*  ᴛɪᴋᴛᴏᴋ
-*┃*  *https://www.tiktok.com/@thelolibot_md*
+*┃*  *https://www.tiktok.com/AGREGAR LINK*
 *┃* ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 *┃*  ғᴀᴄᴇʙᴏᴏᴋ
-*┃* *https://facebook.com/groups/872989990425789/*
+*┃* *LINK DE FACEBOOK*
 *┃* ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 *┃* ᴀɢʀᴀᴄɪᴍɪᴇɴᴛᴏs:
-*┃* ɴᴏᴠᴀʙᴏᴛ-ᴍᴅ ᴛɪᴇɴᴇ ғᴜɴᴄɪᴏɴᴇs
+*┃* SaraBot ᴛɪᴇɴᴇ ғᴜɴᴄɪᴏɴᴇs
 *┃* ǫᴜᴇ ᴘᴇʀᴛᴇɴᴇᴢᴄᴀɴ
 *┃* ᴀ ʟᴀs sɪɢᴜᴇɴᴛᴇs ᴇᴍᴘʀᴇsᴀ(s)|ᴏʀɢᴀɴɪᴢᴀᴄɪᴏɴ(ᴇs)
 *┃* ᴘᴇʀsᴏɴᴀ(s):
 *┃* ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-*┃* ɢᴀᴛᴀʙᴏᴛ-ᴍᴅ
-*┃* *https://github.com/GataNina-Li/GataBot-MD*
+*┃* DjDarianRomano
+*┃* *https://github.com/DjDarian*
 *┃*  ɢʀᴀᴄɪᴀs!!!
 *┗ ┅ ━━━━━━━━━ ┅ ━*
 `
 let img = 'https://i.ibb.co/37FP2bk/donate.jpg'
-conn.sendButton(m.chat, don, 'NovaBot-MD', img, [['GRUPO', `${usedPrefix}grupos`]],m, rpyp)
+conn.sendButton(m.chat, don, 'SaraBot', img, [['GRUPO', `${usedPrefix}grupos`]],m, rpyp)
 }
 
 handler.help = ['donate']
